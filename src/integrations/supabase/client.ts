be79a8edd287bc19382abdf2908414787e42ada1
yannickrastogi <yannick.rastogi@gmail.com>
2026-03-11 18:16:23 -0400

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://iejmrufqtqdwdsywbnsm.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Wg4TNL2dS0EWmnWRnn4VZA_wkJiBdDM";
+const SUPABASE_URL = "https://xhmeitivkclbeziqavxw.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_d5KUqa-Lr4kNcFO4NuXieg_LL4TZQyb";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
